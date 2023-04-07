cb566de26e8d0983d4dc7a6885d2535f6d8d4c96
highspeed12 <suraj.171245@ncit.edu.np>
2023-04-07 22:32:08 +0545

    module i,port remove

--- job/views.py
+++ job/views.py
@@ -3,7 +3,6 @@ from django.shortcuts import render,redirect
 from .models import *
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
-from datetime import date
 
 # Create your views here.
 def index(request):
