943d05ac629a055d1b1cf85f747ae21b2dd32f7c
highspeed12 <suraj.171245@ncit.edu.np>
2023-04-07 22:47:14 +0545

    assss

--- job/views.py
+++ job/views.py
@@ -1,8 +1,9 @@
-#checking the push command of gitgit 
+#checking the push command of gitgit
 from django.shortcuts import render,redirect 
 from .models import *
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
+from datetime import date
 
 # Create your views here.
 def index(request):
