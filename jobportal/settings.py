884e4703a8c6ede4e5cc16ba906d8478164d46b3
Suraj shahi <80339587+Surajshahi2020@users.noreply.github.com>
2024-12-07 10:40:38 +0545

    Update settings.py
    secret key removed

--- jobportal/settings.py
+++ jobportal/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-x-8r-fy*$^%^4g9o4f3ae5*!n8*i867#213$-88&=!x18t-x*p'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
